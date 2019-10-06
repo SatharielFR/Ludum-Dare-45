1b7924b22ab9aaa810c06b00085e2d9c07dc8bc2
Léo Viguier <leo.vgr@hotmail.com>
2019-10-06 17:41:21 -0400

    NPc bouge

--- Source/GetRekt/Npc.cpp
+++ Source/GetRekt/Npc.cpp
@@ -2,12 +2,32 @@
 
 
 #include "Npc.h"
+#include "Components/BoxComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/Engine.h"
+#include "UObject/UObjectGlobals.h"
+#include "UObject/ConstructorHelpers.h"
+#include "UObject/UObjectBaseUtility.h"
+
 
 // Sets default values
-ANpc::ANpc()
+ANpc::ANpc() : iVitesseMax(10), vCurrentDirection(FVector(10, 0, 0))
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// create trigger capsule
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Box"));
+	TriggerBox->InitBoxExtent(FVector(50, 50, 50));
+	TriggerBox->SetBoxExtent(FVector(50, 50, 50), true);
+	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+	TriggerBox->SetupAttachment(RootComponent);
+
+	//TriggerBox->AddToRoot();
+	/*
+	MyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
+	RootComponent = MyMesh;*/
+
 
 }
 
@@ -16,8 +36,14 @@ ANpc::ANpc()
 void ANpc::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, TEXT("OnOverlapBegin"));
+	// declare overlap events
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ANpc::OnOverlapBegin);
+	//TriggerBox->OnComponentHit.AddDynamic(this, &ANpc::OnComponentHit);    // set up a notification for when this component overlaps something
+
+
+
+
 }
 
 // Called every frame
@@ -25,37 +51,106 @@ void ANpc::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	int X = this->GetActorLocation().X;
-	int Y = this->GetActorLocation().Y;
-	int Z = this->GetActorLocation().Z;
-	this->SetActorLocation(FVector(X+10, Y, Z), false, false);
+	int X = this->GetActorLocation().X + this->getCurrentDirection().X;
+	int Y = this->GetActorLocation().Y + this->getCurrentDirection().Y;
+	int Z = this->GetActorLocation().Z + this->getCurrentDirection().Z;
+
+	this->SetActorLocation(FVector(X, Y, Z), false, false);
+
 
 }
+
+void ANpc::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	this->ChangeNpcDirection();
+
+}
+
 
 // method used to get a new random direction 
-FVector ANpc::ChangeNpcDirection(FVector currentDirection)
+void ANpc::ChangeNpcDirection()
 {
-	int iRandom = rand() % 3;
-	FVector nextDirection = FVector(0, 0, 0);
-	do {
-		switch (iRandom) {
-		case 0: // up direction
-			nextDirection = FVector(1, 0, 0);
-			break;
-		case 1: // down direction 
-			nextDirection = FVector(-1, 0, 0);
-			break;
-		case 2: // right direction
-			nextDirection = FVector(1, 0, 0);
-			break;
-		case 3: // left direction
-			nextDirection = FVector(-1, 0, 0);
-			break;
-		default:
-			nextDirection = FVector(0, 0, 0);
-			break;
-		}
 
-	} while (nextDirection==currentDirection);
-	return FVector();
+	FVector nextDirection = FVector(0, 0, 0);
+
+	if (this->getCurrentDirection().X != 0) {
+		if (this->getCurrentDirection().X > 0) {
+			this->SetActorLocation(FVector(this->GetActorLocation().X - 10, this->GetActorLocation().Y, this->GetActorLocation().Z));
+			int iRandom = rand() % 2;
+			switch (iRandom) {
+			case 0:
+				nextDirection = FVector(-this->getVitesseMax(), 0, 0);
+				break;
+			case 1:
+				nextDirection = FVector(0, 0, this->getVitesseMax());
+				break;
+			case 2:
+				nextDirection = FVector(0, 0, -this->getVitesseMax());
+				break;
+			default:
+				nextDirection = FVector(0, 0, 0);
+				break;
+			}
+		}
+		else {
+			this->SetActorLocation(FVector(this->GetActorLocation().X + 10, this->GetActorLocation().Y, this->GetActorLocation().Z));
+			int iRandom = rand() % 2;
+			switch (iRandom) {
+			case 0:
+				nextDirection = FVector(this->getVitesseMax(), 0, 0);
+				break;
+			case 1:
+				nextDirection = FVector(0, 0, this->getVitesseMax());
+				break;
+			case 2:
+				nextDirection = FVector(0, 0, -this->getVitesseMax());
+				break;
+			default:
+				nextDirection = FVector(0, 0, 0);
+				break;
+			}
+		}
+	}
+	else {
+		if (this->getCurrentDirection().Z > 0) {
+			this->SetActorLocation(FVector(this->GetActorLocation().X, this->GetActorLocation().Y, this->GetActorLocation().Z - 10));
+			int iRandom = rand() % 2;
+			switch (iRandom) {
+			case 0:
+				nextDirection = FVector(this->getVitesseMax(), 0, 0);
+				break;
+			case 1:
+				nextDirection = FVector(0, 0, -this->getVitesseMax());
+				break;
+			case 2:
+				nextDirection = FVector(this->getVitesseMax(), 0, 0);
+				break;
+			default:
+				nextDirection = FVector(0, 0, 0);
+				break;
+			}
+		}
+		else {
+			this->SetActorLocation(FVector(this->GetActorLocation().X, this->GetActorLocation().Y, this->GetActorLocation().Z + 10));
+			int iRandom = rand() % 2;
+			switch (iRandom) {
+			case 0:
+				nextDirection = FVector(this->getVitesseMax(), 0, 0);
+				break;
+			case 1:
+				nextDirection = FVector(0, 0, this->getVitesseMax());
+				break;
+			case 2:
+				nextDirection = FVector(this->getVitesseMax(), 0, 0);
+				break;
+			default:
+				nextDirection = FVector(0, 0, 0);
+				break;
+			}
+		}
+	}
+
+	this->setCurrentDirection(nextDirection);
 }
+
--- Source/GetRekt/Npc.h
+++ Source/GetRekt/Npc.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "Npc.generated.h"
+
 
 UCLASS()
 
@@ -16,12 +19,14 @@ class GETREKT_API ANpc : public AActor
 private:
 	//current direction of the NPC
 	FVector vCurrentDirection;
+	int iVitesseMax;
 	 
 	
 public:	
 	// Sets default values for this actor's properties
 	ANpc();
-	FVector ChangeNpcDirection(FVector currentDirection);
+	void ChangeNpcDirection();
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -30,6 +35,28 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	// create mesh 
+	UPROPERTY(VisibleAnywhere, Category = "MyMesh")
+		class UStaticMeshComponent* MyMesh;
+
+	// create trigger capsule
+	UPROPERTY(VisibleAnywhere, Category = "Trigger Capsule")
+		class UBoxComponent* TriggerBox;
+	
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//get
+	FVector getCurrentDirection() { return vCurrentDirection; };
+	int getVitesseMax() { return iVitesseMax; }
+
+	//set
+	void setCurrentDirection(FVector vNewDirection) { vCurrentDirection = vNewDirection; }
+	void setVitesseMax(int iNewVitesse) { iVitesseMax = iNewVitesse; }
+
+
 	
 
 };
